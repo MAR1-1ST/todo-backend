82119ffc6b894e1c39deef22a68358c0d9edb33f
Random-moon <brainmotivation9@gmail.com>
2026-01-03 00:26:21 +0100

    fix: add missing frontend link

--- src/index.js
+++ src/index.js
@@ -77,6 +77,6 @@ process.on('SIGTERM', async () => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0",() => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
